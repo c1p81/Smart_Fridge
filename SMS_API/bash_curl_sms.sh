e792caa186227496612a8f842420d2434898ebce
c1p81 <lucainnoc@gmail.com>
2020-07-03 21:56:42 +0200

    Fix Sms

--- SMS_API/bash_curl_sms.sh
+++ SMS_API/bash_curl_sms.sh
@@ -1,1 +1,1 @@
-curl -s -X POST -H "Content-Type: application/json" --data-binary @test2_sms.json -H "apikey: kepwmQuI3wvtjifH7yt0g2yfnhqTMxl4"  https://hackathon.tim.it/sms/mt 
+curl -s -X POST -H "Content-Type: application/json" --data-binary @test2_sms.json -H "apikey: xxxxxxxxxxxxxxxxxx"  https://hackathon.tim.it/sms/mt 
